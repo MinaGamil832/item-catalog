bfef99346033279c8c7f55c1655dca427599d2d9
Mohamed Edrees <mohamed.edrees0@gmail.com>
2018-08-15 15:06:23 +0200

    change the user to grader at all files

--- project.py
+++ project.py
@@ -25,7 +25,7 @@ APPLICATION_NAME = "Restaurant Menu Application"
 
 
 # Connect to Database and create database session
-engine = create_engine('postgresql://grader:11111@localhost/catalog')
+engine = create_engine('postgresql://catalog:11111@localhost/catalog')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
